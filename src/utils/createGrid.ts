4d4ac88d176ae6972da99b6d15af6569ff8a5325
Thomas <93293510+ta-torres@users.noreply.github.com>
2025-02-06 13:39:25 -0300

    Rename createGrid to .ts and add type annotations

--- src/utils/createGrid.ts
+++ src/utils/createGrid.ts
@@ -1,7 +1,9 @@
-export const createGrid = (rows, cols) => {
-  const grid = [];
+import { NodeType } from "../types";
+
+export const createGrid = (rows: number, cols: number) => {
+  const grid: NodeType[][] = [];
   for (let row = 0; row < rows; row++) {
-    const currentRow = [];
+    const currentRow: NodeType[] = [];
     for (let col = 0; col < cols; col++) {
       currentRow.push({
         row,
